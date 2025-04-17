a8905879989652a913452f04758ff2a9baa722e7
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-04-17 06:54:36 -0700

    created auth routes/layout and social auth form

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({
         enableSystem
         disableTransitionOnChange
       >
-        <Navbar />
         {children}
       </ThemeProvider>
       </body>
