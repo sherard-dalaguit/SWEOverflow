35a8cb68e46901878d56b4864c1c2ec74af2c5d4
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-04-21 14:16:02 -0700

    Created Auth Form For Sign In and Sign Up

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import {SignInSchema} from "@/lib/validations";
+
 const SignIn = () => {
 	return (
-		<div>SignIn</div>
-	)
+		<AuthForm
+			formType="SIGN_IN"
+			schema={SignInSchema}
+			defaultValues={{ email: '', password: '' }}
+			onSubmit={(data) => Promise.resolve({ success: true, data})}
+		/>
+	);
 }
 
 export default SignIn;
