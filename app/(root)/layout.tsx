d5b37d21154d1c3c81897cd0c40a6a9fe731a25e
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-06-05 17:52:25 -0700

    Implemented Right Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/navigation/navbar";
 import {ReactNode} from "react";
 import LeftSidebar from "@/components/navigation/LeftSidebar";
+import RightSidebar from "@/components/navigation/RightSidebar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
 	return (
@@ -8,9 +9,12 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
 			<Navbar />
 			<div className="flex">
 				<LeftSidebar />
+
 				<section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
 					<div className="mx-auto w-full max-w-5xl">{children}</div>
 				</section>
+
+				<RightSidebar />
 			</div>
 		</main>
 	)
