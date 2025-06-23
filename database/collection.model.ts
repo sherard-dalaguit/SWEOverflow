3f0e7e8d51466da195486759f5c3ce28db003720
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-06-22 18:48:04 -0700

    Created Collection Model

---
+++ database/collection.model.ts
@@ -0,0 +1,18 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface ICollection {
+	author: Types.ObjectId;
+	question: Types.ObjectId;
+}
+
+const CollectionSchema = new Schema(
+	{
+		author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+		id: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+	},
+	{ timestamps: true }
+);
+
+const Collection = models?.Collection || model<ICollection>("Collection", CollectionSchema);
+
+export default Collection;
