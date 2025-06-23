ae3786b4460bdc8dac5cabf0d0881d1269ebe85a
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-06-22 18:18:34 -0700

    Created Question Model

---
+++ database/question.model.ts
@@ -0,0 +1,30 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IQuestion {
+	title: string;
+	content: string;
+	tags: Types.ObjectId[];
+	authorId: Types.ObjectId;
+	views: number;
+	answers: number;
+	upvotes: number;
+	downvotes: number;
+}
+
+const QuestionSchema = new Schema(
+	{
+		title: { type: String, required: true },
+		content: { type: String, required: true },
+		tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+		authorId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+		views: { type: Number, default: 0 },
+		answers: { type: Number, default: 0 },
+		upvotes: { type: Number, default: 0 },
+		downvotes: { type: Number, default: 0 },
+	},
+	{ timestamps: true }
+);
+
+const Question = models?.Question || model<IQuestion>("Question", QuestionSchema);
+
+export default Question;
