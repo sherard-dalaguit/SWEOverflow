08273fabd6ec6d81a6cfc84b35ee5f9b9cdc430c
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-06-24 08:16:14 -0700

    Implemented Dynamic Account Routes

---
+++ app/api/accounts/[id]/route.ts
@@ -0,0 +1,63 @@
+import {NotFoundError, ValidationError} from "@/lib/http-errors";
+import handleError from "@/lib/handlers/error";
+import {APIErrorResponse} from "@/types/global";
+import dbConnect from "@/lib/mongoose";
+import {NextResponse} from "next/server";
+import Account from "@/database/account.model";
+import {AccountSchema} from "@/lib/validations";
+
+// GET /api/accounts/[id]
+export async function GET(_: Request, { params }: { params: Promise<{ id: string }>}) {
+	const { id } = await params;
+	if (!id) throw new NotFoundError("Account");
+
+	try {
+		await dbConnect();
+
+		const account = await Account.findById(id);
+		if (!account) throw new NotFoundError("Account");
+
+		return NextResponse.json({ success: true, data: account }, { status: 200 });
+	} catch (error) {
+		return handleError(error, "api") as APIErrorResponse;
+	}
+}
+
+// DELETE /api/accounts/[id]
+export async function DELETE(_: Request, { params }: { params: Promise<{ id: string }>}) {
+	const { id } = await params;
+	if (!id) throw new NotFoundError("Account");
+
+	try {
+		await dbConnect();
+
+		const account = await Account.findByIdAndDelete(id);
+		if (!account) throw new NotFoundError("Account");
+
+		return NextResponse.json({ success: true, data: account }, { status: 200 });
+	} catch (error) {
+		return handleError(error, "api") as APIErrorResponse;
+	}
+}
+
+// PUT /api/accounts/[id]
+export async function PUT(request: Request, { params }: { params: Promise<{ id: string }>}) {
+	const { id } = await params;
+	if (!id) throw new NotFoundError("Account");
+
+	try {
+		await dbConnect();
+
+		const body = await request.json();
+		const validatedData = AccountSchema.partial().safeParse(body);
+
+		if (!validatedData.success) throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+		const updatedAccount = await Account.findByIdAndUpdate(id, validatedData, { new: true });
+		if (!updatedAccount) throw new NotFoundError("Account");
+
+		return NextResponse.json({ success: true, data: updatedAccount }, { status: 200 });
+	} catch (error) {
+		return handleError(error, "api") as APIErrorResponse;
+	}
+}
