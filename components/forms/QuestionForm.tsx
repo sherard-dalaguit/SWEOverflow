1b79704c4bc9cfeb4c0599b8e244fbbd723dd94e
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-06-29 16:26:48 -0700

    Implemented Edit and Get Server Actions and Implementation

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -11,17 +11,23 @@ import {MDXEditorMethods} from "@mdxeditor/editor";
 import dynamic from "next/dynamic";
 import {z} from "zod";
 import TagCard from "@/components/cards/TagCard";
-import {createQuestion} from "@/lib/actions/question.action";
+import {createQuestion, editQuestion} from "@/lib/actions/question.action";
 import {toast} from "sonner";
 import {useRouter} from "next/navigation";
 import ROUTES from "@/constants/routes";
 import {ReloadIcon} from "@radix-ui/react-icons";
+import {Question} from "@/types/global";
 
 const Editor = dynamic(() => import('@/components/editor'), {
   ssr: false
 })
 
-const QuestionForm = () => {
+interface Params {
+	question?: Question;
+	isEdit?: boolean;
+}
+
+const QuestionForm = ({question, isEdit = false}: Params) => {
 	const router = useRouter();
 	const editorRef = useRef<MDXEditorMethods>(null);
 	const [isPending, startTransition] = useTransition();
@@ -29,9 +35,9 @@ const QuestionForm = () => {
 	const form = useForm<z.infer<typeof AskQuestionSchema>>({
 		resolver: zodResolver(AskQuestionSchema),
 		defaultValues: {
-			title: '',
-			content: '',
-			tags: []
+			title: question?.title || '',
+			content: question?.content || '',
+			tags: question?.tags.map((tag) => tag.name) || [],
 		}
 	});
 
@@ -73,6 +79,22 @@ const QuestionForm = () => {
 
 	const handleCreateQuestion = async (data: z.infer<typeof AskQuestionSchema>) => {
 		startTransition(async () => {
+			if (isEdit && question) {
+				const result = await editQuestion({ questionId: question?._id, ...data });
+
+				if (result.success) {
+					toast.success("Success", { description: "Question updated successfully!" });
+
+					if (result.data) router.push(ROUTES.QUESTION(result.data._id));
+				} else {
+					toast.error(`Error ${result.status}`, {
+						description: result.error?.message || "Failed to edit question."
+					});
+				}
+
+				return;
+			}
+
 			const result = await createQuestion(data);
 
 			if (result.success) {
@@ -187,7 +209,7 @@ const QuestionForm = () => {
 								<span>Submitting</span>
 							</>
 						) : (
-							<>Ask A Question</>
+							<>{isEdit ? "Edit" : "Ask A Question"}</>
 						)}
 					</Button>
 				</div>
--- types/global.d.ts
+++ types/global.d.ts
@@ -12,6 +12,7 @@ interface Author {
 export interface Question {
 	_id: string;
 	title: string;
+	content: string;
 	tags: Tag[];
 	author: Author;
 	createdAt: Date;
